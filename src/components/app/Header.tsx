654e134968cf0276d847a1adbfe164156c247c2f
Aayush Goyal <aayushgoyal.mps@gmail.com>
2025-06-27 00:46:08 +0530

    feat: add Header component with logout and top level navigation functionality
    Signed-off-by: Aayush Goyal <aayushgoyal.mps@gmail.com>

---
+++ src/components/app/Header.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+// Import global from third party libraries.
+import Link from 'next/link';
+
+// Import custom components.
+import { useRouter } from 'next/navigation';
+import { Button } from '@/src/components/ui/button';
+
+// Import custom utilities.
+
+// Import custom types.
+
+// Import styles.
+
+/**
+ * Description
+ */
+export default function Header() {
+    // SECTION: States and Constants
+    const router = useRouter();
+    // !SECTION
+
+    // SECTION: API Queries
+    // !SECTION
+
+    // SECTION: Event Handlers
+    const handleLogout = () => {
+        // Clear authentication details from local storage
+        localStorage.removeItem('authDetails');
+        // Clear favorites from local storage
+        localStorage.removeItem('favorites');
+        // Redirect to login page
+        router.push('/login');
+    };
+    // !SECTION
+
+    // SECTION: Side Effects
+    // !SECTION
+
+    // SECTION: UI
+    // !SECTION
+    return (
+        <div className="flex w-full items-center justify-between bg-ash-gray-light px-8 py-2">
+            <div className="flex items-center space-x-8">
+                <Link href="/">
+                    <p className="group relative">
+                        Home
+                        <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full" />
+                    </p>
+                </Link>
+                <Link href="/favorites">
+                    <p className="group relative">
+                        Favorites
+                        <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full" />
+                    </p>
+                </Link>
+            </div>
+            <Button variant="link" onClick={() => handleLogout()}>
+                LOGOUT
+            </Button>
+        </div>
+    );
+}
